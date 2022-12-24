44ab2312567940e8a5cddeedc617dc688a46faf1
YahyaHassan108 <121357397+YahyaHassan108@users.noreply.github.com>
2022-12-24 22:47:07 +0200

    Add files via upload

---
+++ Sorting/Sorting/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.InsertionSortRB = new System.Windows.Forms.RadioButton();
+            this.MergeSortRB = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Choose Sorting Algorithm";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(230, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(511, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // InsertionSortRB
+            // 
+            this.InsertionSortRB.AutoSize = true;
+            this.InsertionSortRB.Location = new System.Drawing.Point(230, 201);
+            this.InsertionSortRB.Name = "InsertionSortRB";
+            this.InsertionSortRB.Size = new System.Drawing.Size(91, 17);
+            this.InsertionSortRB.TabIndex = 3;
+            this.InsertionSortRB.TabStop = true;
+            this.InsertionSortRB.Text = "Insertion Sort";
+            this.InsertionSortRB.UseVisualStyleBackColor = true;
+            // 
+            // MergeSortRB
+            // 
+            this.MergeSortRB.AutoSize = true;
+            this.MergeSortRB.Location = new System.Drawing.Point(464, 199);
+            this.MergeSortRB.Name = "MergeSortRB";
+            this.MergeSortRB.Size = new System.Drawing.Size(78, 17);
+            this.MergeSortRB.TabIndex = 4;
+            this.MergeSortRB.TabStop = true;
+            this.MergeSortRB.Text = "Merge Sort";
+            this.MergeSortRB.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 341);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(682, 81);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(227, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "*Separate with white space";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(227, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(316, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "*If no Sorting algorithm is choosed, output will equale input data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MergeSortRB);
+            this.Controls.Add(this.InsertionSortRB);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton InsertionSortRB;
+        private System.Windows.Forms.RadioButton MergeSortRB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Sorting/Sorting/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string ArrToStr(int[] array)
+        {
+            String Output = "" ;
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                Output += array[i] + " , ";
+            }
+            return Output;
+        }
+
+        public int[] InsertionSort(int[] array, int length)
+        {
+            for (int i = 1; i < length; i++)
+            {
+                var key = array[i];
+                var flag = 0;
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (key < array[j])
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                        array[j + 1] = key;
+                    }
+                    else flag = 1;
+                }
+            }
+            return array;
+        }
+
+
+        public int[] MergeSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                MergeSort(array, left, middle);
+                MergeSort(array, middle + 1, right);
+                MergeArray(array, left, middle, right);
+            }
+            return array;
+        }
+
+        public void MergeArray(int[] array, int left, int middle, int right)
+        {
+            var leftArrayLength = middle - left + 1;
+            var rightArrayLength = right - middle;
+            var leftTempArray = new int[leftArrayLength];
+            var rightTempArray = new int[rightArrayLength];
+            int i, j;
+            for (i = 0; i < leftArrayLength; ++i)
+                leftTempArray[i] = array[left + i];
+            for (j = 0; j < rightArrayLength; ++j)
+                rightTempArray[j] = array[middle + 1 + j];
+            i = 0;
+            j = 0;
+            int k = left;
+            while (i < leftArrayLength && j < rightArrayLength)
+            {
+                if (leftTempArray[i] <= rightTempArray[j])
+                {
+                    array[k++] = leftTempArray[i++];
+                }
+                else
+                {
+                    array[k++] = rightTempArray[j++];
+                }
+            }
+            while (i < leftArrayLength)
+            {
+                array[k++] = leftTempArray[i++];
+            }
+            while (j < rightArrayLength)
+            {
+                array[k++] = rightTempArray[j++];
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> myNumbers = new List<int>();
+            int i;
+            foreach (string str in textBox1.Text.Split(' '))
+            {
+
+                if (int.TryParse(str, out i))
+                    myNumbers.Add(int.Parse(str));
+
+            }
+
+            int[] myNum = myNumbers.ToArray();
+
+            if (InsertionSortRB.Checked == true)
+            {
+                InsertionSort(myNum, myNum.Length);
+            }
+
+            else if (MergeSortRB.Checked == true)
+            {
+                MergeSort(myNum, 0, myNum.Length - 1);
+            }
+
+            textBox2.Text = ArrToStr(myNum);
+        }
+    }
+}
